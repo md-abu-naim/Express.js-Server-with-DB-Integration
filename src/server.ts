02677ff778c94a8e0abc0fc4b00bb1b72fbbf9fc
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-15 21:11:36 +0600

    Getting All Users and Single user with params

--- src/server.ts
+++ src/server.ts
@@ -35,14 +35,14 @@ const initDB = async () => {
 
 initDB()
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api/', (req: Request, res: Response) => {
     res.status(200).json({
         "message": "Express Server",
         "author": "Mohammad Abu Naim"
     })
 })
 
-app.post('/post', async (req: Request, res: Response) => {
+app.post('/api/users', async (req: Request, res: Response) => {
     try {
         const { name, age, email, password } = req.body
 
@@ -55,6 +55,54 @@ app.post('/post', async (req: Request, res: Response) => {
             message: "Created Post",
             data: result.rows[0]
         })
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message,
+            error: error
+        })
+    }
+})
+
+app.get('/api/users', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users
+            `)
+
+        res.status(200).json({
+            success: true,
+            message: "Users retrived successfully",
+            data: result.rows,
+        })    
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message,
+            error: error
+        })
+    }
+})
+
+app.get('/api/users/:id', async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params
+        const result = await pool.query(`
+            SELECT * FROM users WHERE id=$1
+            `, [id])
+
+        if(result.rows?.length === 0){
+            res.status(500).json({
+            success: false,
+            message: "User not found",
+            data: {},
+        })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User retrived successfully",
+            data: result.rows[0],
+        })
+        
     } catch (error: any) {
         res.status(500).json({
             message: error.message,
